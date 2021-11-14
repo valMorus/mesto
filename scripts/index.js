188a1edad270bcd6add93daaebaadb0cd13207f5
Val Morus <lera.parus@yandex.ru>
2021-11-14 18:50:33 +0500

    прописать дефолтные поля в попапе

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,30 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const nameElement = document.querySelector('.profile__name'); 
+const jobElement = document.querySelector('.profile__job'); 
+const formElement = document.querySelector('.popup__content');
+const nameInput = formElement.querySelector('.popup__name');
+const jobInput = formElement.querySelector('.popup__job');
 
 const openPopup = function() {
     popupElement.classList.add('popup_is-opened')
+if (popupElement.classList.contains('popup_is-opened')){
+    nameInput.value = nameElement.textContent;
+    jobInput.value = jobElement.textContent;
+}
 };
 
 const closePopup = function() {
     popupElement.classList.remove('popup_is-opened')
 };
 
-//togglePopupVisibility();
-
 popupOpenButtonElement.addEventListener('click', openPopup);
 
 popupCloseButtonElement.addEventListener('click', closePopup);
+
+
+
+
+
+
